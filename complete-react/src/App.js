7a48e5c56f160b0a9e956224cff9960dcc824be9
MonalisaBasa <monalisa.basa.mb@gmail.com>
2023-07-06 18:01:13 +0530

    React basic

---
+++ complete-react/src/App.js
@@ -0,0 +1,15 @@
+import Expenseitem from './Expenseitem.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <Expenseitem></Expenseitem>
+    </div>
+  );
+}
+
+export default App;
