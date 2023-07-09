9c721dc0fddd7329447e6da61e820c4eec316644
MonalisaBasa <monalisa.basa.mb@gmail.com>
2023-07-10 00:05:01 +0530

    DOM react states

--- complete-react/src/EXPENSES/Expenseitem.js
+++ complete-react/src/EXPENSES/Expenseitem.js
@@ -1,12 +1,26 @@
 import ExpenseDate from "./ExpenseDate.js";
 import ExpenseDetails from "./ExpenseDetails.js";
+import {useState} from 'react';
 const Expenseitem = (props) =>{
+    // const clickHandler=()=>{
+    //     console.log("clicked!!");
+    // }
+    const [amt,setAmt]=useState(props.amount)
+    const clickHandler=()=>{
+        setAmt("100");
+        console.log(amt);
+
+    }
+
   
     return(
         <div className="expense-item">
             
             <ExpenseDate date={props.date}/>
-            <ExpenseDetails title={props.title} amount={props.amount}/>
+            <ExpenseDetails title={props.title} amount={amt}/>
+            {/* <button onClick={clickHandler}>Delete Expenses</button> */}
+            <button onClick={clickHandler}>Change Expenses</button>
+
            
     </div>
     );
