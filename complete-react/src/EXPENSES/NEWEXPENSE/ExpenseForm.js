637d98370a411d2404105eca42454d3f846faa14
MonalisaBasa <monalisa.basa.mb@gmail.com>
2023-07-10 21:42:42 +0530

    Introduce State inisde component

--- complete-react/src/EXPENSES/NEWEXPENSE/ExpenseForm.js
+++ complete-react/src/EXPENSES/NEWEXPENSE/ExpenseForm.js
@@ -1,7 +1,20 @@
-const Expenseform=()=>{
-    const Changehandler = (event)=>{
-        console.log(event.target.value);
+import React,{useState } from 'react';
+const Expenseform=(props)=>{
+    // const Changehandler = (event)=>{
+    //     console.log(event.target.value);
 
+    // }
+    const[title,setTitle]=useState('');
+    const[amount,setAmount]=useState('');
+    const[date,setDate]=useState('');
+    const titleChangeHandler=(e)=>{
+        setTitle(e.target.value);
+    }
+    const amountChangeHandler = (e) =>{
+        setAmount(e.target.value);
+    }
+    const dateChangeHandler = (e) =>{
+        setDate(e.target.value);
     }
     return(
         <form >
@@ -9,15 +22,16 @@ const Expenseform=()=>{
                 <div class="new-expense">
                     <label >Title</label>
                     {/* onchange event handler */}
-                    <input type="text" onChange={Changehandler} />   
+                    {/* <input type="text" onChange={Changehandler} />    */}
+                    <input type="text" onChange={titleChangeHandler} />
                 </div>
                 <div class="new-expense">
                     <label >Amount</label>
-                    <input type="number" min="0.01" step="0.01"/>
+                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
                 </div>
                 <div class="new-expense">
                     <label >Date</label>
-                    <input type="date" min="2019-01-01" step="2022-12-31" />
+                    <input type="date" min="2019-01-01" step="2022-12-31" onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div>
