ae850d9b24fceae67139201b236ee1bcbaba0712
MonalisaBasa <monalisa.basa.mb@gmail.com>
2023-07-11 10:29:37 +0530

    submitting forms

--- complete-react/src/EXPENSES/NEWEXPENSE/ExpenseForm.js
+++ complete-react/src/EXPENSES/NEWEXPENSE/ExpenseForm.js
@@ -1,23 +1,37 @@
-import React,{useState } from 'react';
-const Expenseform=(props)=>{
+import React, { useState } from 'react';
+const Expenseform = (props) => {
     // const Changehandler = (event)=>{
     //     console.log(event.target.value);
 
     // }
-    const[title,setTitle]=useState('');
-    const[amount,setAmount]=useState('');
-    const[date,setDate]=useState('');
-    const titleChangeHandler=(e)=>{
+    const [Enteredtitle, setTitle] = useState('');
+    const [Enteredamount, setAmount] = useState('');
+    const [Entereddate, setDate] = useState('');
+    const titleChangeHandler = (e) => {
         setTitle(e.target.value);
     }
-    const amountChangeHandler = (e) =>{
+    const amountChangeHandler = (e) => {
         setAmount(e.target.value);
     }
-    const dateChangeHandler = (e) =>{
+    const dateChangeHandler = (e) => {
         setDate(e.target.value);
     }
-    return(
-        <form >
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        const obj = {
+            title: Enteredtitle,
+            amount: Enteredamount,
+            date: new Date(Entereddate)
+
+        }
+        console.log(obj);
+    }
+
+
+    return (
+        <form onSubmit={submitHandler}>
             <div class="new-expense">
                 <div class="new-expense">
                     <label >Title</label>
@@ -27,11 +41,11 @@ const Expenseform=(props)=>{
                 </div>
                 <div class="new-expense">
                     <label >Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
                 </div>
                 <div class="new-expense">
                     <label >Date</label>
-                    <input type="date" min="2019-01-01" step="2022-12-31" onChange={dateChangeHandler}/>
+                    <input type="date" min="2019-01-01" step="2022-12-31" onChange={dateChangeHandler} />
                 </div>
             </div>
             <div>
