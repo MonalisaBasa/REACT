4f98fbbb12b24eb0d40745ead2166d5a58f06948
MonalisaBasa <monalisa.basa.mb@gmail.com>
2023-07-11 22:29:14 +0530

    stateful and stateless component

--- complete-react/src/EXPENSES/Expenseitem.js
+++ complete-react/src/EXPENSES/Expenseitem.js
@@ -1,25 +1,27 @@
 import ExpenseDate from "./ExpenseDate.js";
 import ExpenseDetails from "./ExpenseDetails.js";
-import {useState} from 'react';
+// import {useState} from 'react';
 const Expenseitem = (props) =>{
     // const clickHandler=()=>{
     //     console.log("clicked!!");
     // }
-    const [amt,setAmt]=useState(props.amount)
-    const clickHandler=()=>{
-        setAmt("100");
-        console.log(amt);
+    // const [amt,setAmt]=useState(props.amount)
+    // const clickHandler=()=>{
+    //     setAmt("100");
+    //     console.log(amt);
 
-    }
+    // }
 
   
     return(
         <div className="expense-item">
             
             <ExpenseDate date={props.date}/>
-            <ExpenseDetails title={props.title} amount={amt}/>
+            {/* <ExpenseDetails title={props.title} amount={amt}/> */}
+          {/* Stateless component */}
+            <ExpenseDetails title={props.title} amount={props.amt}/>
             {/* <button onClick={clickHandler}>Delete Expenses</button> */}
-            <button onClick={clickHandler}>Change Expenses</button>
+            <button >Change Expenses</button>
 
            
     </div>
