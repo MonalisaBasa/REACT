7259ee0e9c35aafdf7f5fe55a0d908d50bcb2e29
MonalisaBasa <monalisa.basa.mb@gmail.com>
2023-07-11 23:49:39 +0530

    rendering Expenses

--- complete-react/src/App.js
+++ complete-react/src/App.js
@@ -1,9 +1,12 @@
 import Expenseitem from './EXPENSES/Expenseitem.js';
 import Newexpense from './NEWEXPENSE/Newexpense.js';
+import Expensefilter from './EXPENSES/Expensefilter.js';
+import React,{ useState } from 'react';
 import './App.css';
+import Expenses from './EXPENSES/Expense.js';
 
-function App() {
-  const Expenses=[
+// function App() {
+  const DUMMY_EXPENSES=[
     {title:"Toilet paper",amount:94.42,date:new Date(2023,7,14)},
     {title:"New Tv",amount:50000,date:new Date(2023,6,14)},
     {title:"Car Insurance",amount:294.42,date:new Date(2023,5,14)},
@@ -11,14 +14,21 @@ function App() {
   
   
   ]
+  const App = ()=>{
+    const[expenses,setExpenses]=useState(DUMMY_EXPENSES);
+  
   const addExpenseHandler = expense =>{
-    console.log('In App.js');
-    console.log(expense);
+    // console.log('In App.js');
+    // console.log(expense);
+    setExpenses((prevExpenses) =>{
+      return [expense,...prevExpenses]
+
+    })
   }
   return (
     <div className="App">
       <header className="App-header"></header>
-      <Expenseitem 
+      {/* <Expenseitem 
       title={Expenses[0].title}
       amount={Expenses[0].amount} 
       date={Expenses[0].date}
@@ -35,11 +45,14 @@ function App() {
       <Expenseitem
       title={Expenses[3].title}
       amount={Expenses[3].amount} 
-      date={Expenses[3].date}></Expenseitem>
+      date={Expenses[3].date}></Expenseitem> */}
 
       <Newexpense onAddExpense={addExpenseHandler}/>
+      <Expenses items={expenses}/>
+      <Expensefilter/>
     </div>
   );
-}
+  }
+// }
 
 export default App;
